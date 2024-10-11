c61edc19ee7375656d9c02bd4b8d9ef3d5059ab9
977 <1808419119@qq.com>
2024-10-11 15:48:56 +0800

    init repo

--- project/net_disk/over-flow_-net-disk_3/sql/user.sql
+++
@@ -1,7 +0,0 @@
--- 创建user表
-CREATE TABLE user (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- 用户ID
-    username VARCHAR(255), -- 用户名
-    salt VARCHAR(20), -- 密码盐值
-    password VARCHAR(255) -- 加密后的密码
-);
