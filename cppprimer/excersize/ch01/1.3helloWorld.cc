2f982e9af917a8e06a54709a636ad3b52b2f8ec5
977 <1808419119@qq.com>
2024-10-12 10:02:36 +0800

    excersize 1.2 1.3 1.4

--- cppprimer/excersize/ch01/1.3helloWorld.cc
+++ cppprimer/excersize/ch01/1.3helloWorld.cc
@@ -1,3 +1,5 @@
+// 练习1.3:
+// 编写程序，在标准输出上打印：Hello，World。
 #include <iostream>
 
 int main()
--- cppprimer/excersize/ch01/returnValue.cc
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    // return -1; // 表示程序执行错误 echo $? 结果为255
-    return 0; // 表示程序执行正常 echo $? 结果为0
-}
